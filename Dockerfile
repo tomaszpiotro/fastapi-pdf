975f10aebf7b9bd47a747d031529118a01e42c95
Tomasz Piotrowski <tomasz.piotrowski2@pkobp.pl>
2021-10-15 19:04:10 +0200

    Functional example.

--- Dockerfile
+++ Dockerfile
@@ -5,15 +5,23 @@ ENV PYTHONUNBUFFERED 1
 RUN mkdir /fastapi-pdf
 WORKDIR /fastapi-pdf
 
-RUN apk update
-RUN apk add --virtual build-dependencies \
-    build-base \
-    wget \
-    git
+RUN apk --no-cache add \
+     bash \
+     curl \
+     gcc \
+     git \
+     linux-headers \
+     build-base
 
-RUN apk add --no-cache python3-dev openssl-dev libffi-dev py3-setuptools && pip3 install --upgrade pip
 
-RUN apk --no-cache add bash
+RUN apk --no-cache add msttcorefonts-installer fontconfig && \
+    update-ms-fonts && \
+    fc-cache -f
+
+RUN apk --update --upgrade add cairo pango gdk-pixbuf py3-cffi py3-pillow
+RUN apk --update --upgrade add musl-dev jpeg-dev zlib-dev libffi-dev cairo-dev pango-dev gdk-pixbuf-dev
+
+RUN apk add --no-cache python3-dev openssl-dev py3-setuptools && pip3 install --upgrade pip
 
 COPY requirements.txt /fastapi-pdf
 RUN pip install -r requirements.txt
--- fastapi-pdf/app.py
+++ fastapi-pdf/app.py
@@ -1,12 +1,79 @@
-from fastapi import FastAPI
-import sys
 import os
+from typing import Optional
 
-debug = os.environ.get('DEBUG', False)
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from weasyprint import HTML
+
+debug = os.environ.get('DEBUG', False,)
 
 app = FastAPI(debug=debug)
 
 
-@app.get('/')
-async def version():
-    return {'version': sys.version}
+templates = Jinja2Templates(directory='templates')
+
+
+class PdfResponse(Response):
+    media_type = 'application/pdf'
+
+
+@app.get('/html-template-example', response_class=HTMLResponse)
+async def html_template_example(
+    request: Request,
+    first_name: Optional[str] = None,
+    last_name: Optional[str] = None,
+    email: Optional[str] = None,
+    phone_number: Optional[str] = None,
+    first_consent: bool = False,
+    second_consent: bool = False,
+    third_consent: bool = False,
+    fourth_consent: bool = False,
+):
+    return templates.TemplateResponse(
+        'example.html',
+        {
+            'request': request,
+            'filename': 'statute.pdf',
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'phone_number': phone_number,
+            'first_consent': first_consent,
+            'second_consent': second_consent,
+            'third_consent': third_consent,
+            'fourth_consent': fourth_consent,
+        },
+    )
+
+
+@app.get('/pdf-template-example', response_class=PdfResponse)
+async def html_template_example(
+    request: Request,
+    first_name: str = None,
+    last_name: str = None,
+    email: str = None,
+    phone_number: str = None,
+    first_consent: bool = False,
+    second_consent: bool = False,
+    third_consent: bool = False,
+    fourth_consent: bool = False,
+):
+    html = templates.TemplateResponse(
+        'example.html',
+        {
+            'request': request,
+            'filename': 'statute.pdf',
+            'first_name': first_name,
+            'last_name': last_name,
+            'email': email,
+            'phone_number': phone_number,
+            'first_consent': first_consent,
+            'second_consent': second_consent,
+            'third_consent': third_consent,
+            'fourth_consent': fourth_consent,
+        },
+    ).body
+    pdf_content = HTML(string=html).write_pdf()
+    return PdfResponse(
+        content=pdf_content, headers={'Content-Disposition': f'inline; filename=statute.pdf'})
